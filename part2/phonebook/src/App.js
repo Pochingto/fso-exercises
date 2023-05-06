3ab103a613c3a2763f1d9bc77b12c09d6aa4b081
Pochingto <rcwk3756@gmail.com>
2023-05-06 22:52:12 +0800

    exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,13 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas',
+      number: '23045'  
+    }
   ]) 
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
   const addPerson = (event) => {
     event.preventDefault()
     const duplicate = persons.find(person => person.name === newName)
@@ -14,9 +18,11 @@ const App = () => {
     }
 
     setPersons(persons.concat({
-      name: newName
+      name: newName,
+      number: newNumber
     }))
     setNewName("")
+    setNewNumber("")
   }
 
   return (
@@ -27,11 +33,14 @@ const App = () => {
           name: <input value={newName} onChange={(event) => setNewName(event.target.value)}/>
         </div>
         <div>
+          number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((person) => <div key={person.name}>{person.name}</div>)}
+      {persons.map((person) => <div key={person.name}>{person.name} {person.number}</div>)}
     </div>
   )
 }
