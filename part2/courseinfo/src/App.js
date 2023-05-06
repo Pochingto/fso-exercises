09d6f4bed81b99f0027d9fe000c2e09beb4bca4f
Pochingto <rcwk3756@gmail.com>
2023-05-06 11:09:26 +0800

    exercise 2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,27 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <b>Total of {sum} exercises</b>
-
-const Part = ({ part }) => 
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
-const Content = ({ parts }) => 
-  <>
-    {
-      parts.map((part) => <Part part={part} key={part.id}/>)
-    }   
-  </>
-
-const Course = ({course}) => {
-  const total = course.parts.reduce((sum, part) => (sum += part.exercises), 0)
-  return <div>
-    <Header course={course.name} />
-    <Content parts={course.parts} />
-    <Total sum={total} />
-  </div>
-}
+import Course from './components/Course'
 
 const App = () => {
   const courses = [
