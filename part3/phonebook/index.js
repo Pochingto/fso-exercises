62cbb317b1d7b2eb49741a3287655835de294e1d
Pochingto <rcwk3756@gmail.com>
2023-05-12 16:59:04 +0800

    exercise 3.3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -36,6 +36,16 @@ app.get("/api/info", (request, response) => {
   )
 })
 
+app.get("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
+  if (person) {
+    response.json(person)
+  }else {
+    response.status(404).end()
+  }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Listening at port ${PORT}`)
