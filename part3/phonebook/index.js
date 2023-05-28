28a37394a9326a8a31b0f09bb8ddcf647e50904e
Pochingto <rcwk3756@gmail.com>
2023-05-28 12:44:08 +0800

    exercise 3.16

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -61,12 +61,13 @@ const generateID = () => {
   return Math.floor(Math.random() * 100000000);
 }
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response, next) => {
     // response.json(persons)
     Person.find({})
       .then(result => {
         response.json(result)
       })
+      .catch(error => next(error))
 })
 
 app.get("/api/info", (request, response) => {
@@ -77,17 +78,20 @@ app.get("/api/info", (request, response) => {
   )
 })
 
-app.get("/api/persons/:id", (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(p => p.id === id)
-  if (person) {
-    response.json(person)
-  }else {
-    response.status(404).end()
-  }
+app.get("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id
+  // const person = persons.find(p => p.id === id)
+  // if (person) {
+  //   response.json(person)
+  // }else {
+  //   response.status(404).end()
+  // }
+  Person.findById(id)
+    .then(result => response.json(result))
+    .catch(error => next(error))
 })
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response, next) => {
   const body = request.body
   if (body.name && body.number) {
     // const duplicate = persons.find(person => person.name === body.name)
@@ -110,6 +114,7 @@ app.post("/api/persons", (request, response) => {
     })
     newPerson.save()
       .then(result => response.json(result))
+      .catch(error => next(error))
   }else {
     response.status(400).json({
       error: "name or number missing"
@@ -117,16 +122,29 @@ app.post("/api/persons", (request, response) => {
   }
 })
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response, next) => {
   const id = request.params.id
   // persons = persons.filter(p => p.id !== id)
   Person.findByIdAndDelete(id)
     .then(result => response.status(204).end())
-
+    .catch(error => next(error))
   // response.status(204).end()
 })
 
 app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.log("error name: ", error.name)
+  console.log("error message: ", error.message)
+  if (error.name === "CastError") {
+    response.status(400).send({
+      "message": "invalid id"
+    })
+  }
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
