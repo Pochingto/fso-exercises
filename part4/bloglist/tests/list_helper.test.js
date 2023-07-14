d292f1a01d8430b4b5803581660fe33fea9fc864
Pochingto <rcwk3756@gmail.com>
2023-07-14 16:47:33 +0800

    exercise 4.6

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -7,6 +7,33 @@ test('dummy returns one', () => {
     expect(result).toBe(1)
 })
 
+const moreBlogs = [
+    {
+        title: 'blog1',
+        author: 'author1',
+        url: 'url1',
+        likes: 1000
+    },
+    {
+        title: 'blog2',
+        author: 'author2',
+        url: 'url2',
+        likes: 2002
+    },
+    {
+        title: 'blog3',
+        author: 'author3',
+        url: 'url3',
+        likes: 3030
+    },
+    {
+        title: 'blog4',
+        author: 'author4',
+        url: 'url4',
+        likes: 4400
+    }
+]
+
 describe('total likes', () => {
     test('of empty list is zero', () => {
         const blogs = []
@@ -28,33 +55,56 @@ describe('total likes', () => {
     })
 
     test('of a bigger blog list', () => {
-        const blogs = [
-            {
-                title: 'blog1',
-                author: 'author1',
-                url: 'url1',
-                likes: 1000
-            },
-            {
-                title: 'blog2',
-                author: 'author2',
-                url: 'url2',
-                likes: 2002
-            },
-            {
-                title: 'blog3',
-                author: 'author3',
-                url: 'url3',
-                likes: 3030
-            },
-            {
-                title: 'blog4',
-                author: 'author4',
-                url: 'url4',
-                likes: 4400
-            }
-        ]
-        const result = listHelper.totalLikes(blogs)
+        const result = listHelper.totalLikes(moreBlogs)
         expect(result).toBe(10432)
+    })
+})
+
+describe('favoriteBlog', () => {
+    test('of a bigger blog list', () => {
+        const result = listHelper.favoriteBlog(moreBlogs)
+        expect(result).toEqual({
+            title: 'blog4',
+            author: 'author4',
+            url: 'url4',
+            likes: 4400
+        })
+    })
+})
+
+const moreAuthorsBlogs = [
+    {
+        title: 'blog1',
+        author: 'author1',
+        url: 'url1',
+        likes: 1000
+    },
+    {
+        title: 'blog2',
+        author: 'author1',
+        url: 'url2',
+        likes: 2002
+    },
+    {
+        title: 'blog3',
+        author: 'author1',
+        url: 'url3',
+        likes: 3030
+    },
+    {
+        title: 'blog4',
+        author: 'author2',
+        url: 'url4',
+        likes: 14400
+    }
+]
+
+describe('Author with the most blogs', () => {
+    test('of a bigger blog list', () => {
+        const result = listHelper.mostBlogs(moreAuthorsBlogs)
+        expect(result).toEqual({
+            author: 'author1',
+            blogs: 3
+        })
     })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -7,7 +7,37 @@ const totalLikes = (blogs) => {
     return blogs.reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+    const reducer = (most, blog) => {
+        if (most === null || blog.likes > most.likes) {
+            most = blog
+        }
+        return most
+    }
+
+    return blogs.reduce(reducer, null)
+}
+
+const mostBlogs = (blogs) => {
+    const reducer = (counter, blog) => {
+        const authorCount = counter.get(blog.author)
+        counter.set(blog.author, authorCount ? authorCount + 1 : 1)
+        return counter
+    }
+    const counter = blogs.reduce(reducer, new Map())
+
+    const mostAuthor = [...counter.entries()].reduce((most, current) => {
+        return current[1] > most[1] ? current : most
+    })
+    return {
+        author: mostAuthor[0],
+        blogs: mostAuthor[1]
+    }
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs
 }
