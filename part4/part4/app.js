6caf9ca374276f40bd4137e7210b8b6d83b2858b
Pochingto <rcwk3756@gmail.com>
2023-07-19 13:19:35 +0800

    content part 4d: added login router

--- part4/part4/app.js
+++ part4/part4/app.js
@@ -9,6 +9,7 @@ const userRouter = require('./controllers/user')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
 const middleware = require('./utils/middleware')
+const loginRouter = require('./controllers/login')
 
 mongoose.set('strictQuery', false)
 const MONGODB_URI = process.env.NODE_ENV === 'test'
@@ -31,6 +32,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 app.use('/api/notes', notesRouter)
 app.use('/api/users', userRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
