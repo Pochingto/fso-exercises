e633c7d7742922c069a755285a03efbf58f00991
Pochingto <rcwk3756@gmail.com>
2023-07-19 12:24:09 +0800

    content part 4c: added populate between User and Note

--- part4/part4/controllers/user.js
+++ part4/part4/controllers/user.js
@@ -3,7 +3,8 @@ const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
 userRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User
+        .find({}).populate('notes', { content: 1, important: 1 })
     return response.json(users)
 })
 
