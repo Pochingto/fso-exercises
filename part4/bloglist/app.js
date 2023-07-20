40f66d51e4c40f38b14a786ab49341ac68beae57
Pochingto <rcwk3756@gmail.com>
2023-07-20 22:46:30 +0800

    exercise 4.16: added validation of username and password

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+require('express-async-errors')
 const cors = require('cors')
 const mongoose = require('mongoose')
 
 const config = require('./utils/config')
+const middleware = require('./utils/middleware')
 const blogRouter = require('./controllers/blog')
 const userRouter = require('./controllers/user')
 
@@ -15,5 +17,7 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', userRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- part4/bloglist/controllers/user.js
+++ part4/bloglist/controllers/user.js
@@ -1,4 +1,5 @@
 const userRouter = require('express').Router()
+const { default: mongoose } = require('mongoose')
 const User = require('../models/user')
 const bcrypt = require('bcrypt')
 
@@ -12,6 +13,11 @@ userRouter.post('/', async (request, response) => {
     if (!body || !body.username || !body.password) {
         return response.status(400).json({
             error: 'request body missing username or password'
+        })
+    }
+    if (body.password.length < 3) {
+        return response.status(400).json({
+            error: 'password must be at least 3 characters long'
         })
     }
 
