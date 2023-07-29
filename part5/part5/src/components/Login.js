8c27cbddfa3cb07e72d238625a281ea06ae82824
Pochingto <rcwk3756@gmail.com>
2023-07-29 10:45:41 +0800

    part 5c: added basic testing

--- part5/part5/src/components/Login.js
+++ part5/part5/src/components/Login.js
@@ -1,36 +1,36 @@
 import PropTypes from 'prop-types'
 
-const LoginForm = ({handleSubmit, handleUserNameChange, handlePasswordChange, username, password}) => (
+const LoginForm = ({ handleSubmit, handleUserNameChange, handlePasswordChange, username, password }) => (
     <div>
-       <h2>Login</h2>
- 
-       <form onSubmit={handleSubmit}>
-         <div>
+        <h2>Login</h2>
+
+        <form onSubmit={handleSubmit}>
+            <div>
            username
-           <input
-             value={username}
-             onChange={handleUserNameChange}
-           />
-         </div>
-         <div>
+                <input
+                    value={username}
+                    onChange={handleUserNameChange}
+                />
+            </div>
+            <div>
            password
-           <input
-             type="password"
-             value={password}
-             onChange={handlePasswordChange}
-           />
-       </div>
-         <button type="submit">login</button>
-       </form>
-     </div>
+                <input
+                    type="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+                />
+            </div>
+            <button type="submit">login</button>
+        </form>
+    </div>
 )
 
 LoginForm.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
-  handleUserNameChange: PropTypes.func.isRequired,
-  handlePasswordChange: PropTypes.func.isRequired,
-  username: PropTypes.string.isRequired,
-  password: PropTypes.string.isRequired
+    handleSubmit: PropTypes.func.isRequired,
+    handleUserNameChange: PropTypes.func.isRequired,
+    handlePasswordChange: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    password: PropTypes.string.isRequired
 }
 
 export default LoginForm
